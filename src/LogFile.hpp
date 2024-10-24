5f10d66c61b9df5ce856443508ee3051d3c64633
Philipp Wollschlegel <folken@kabelsalat.ch>
2024-10-24 22:23:26 +0200

    null change

--- src/LogFile.hpp
+++ src/LogFile.hpp
@@ -25,12 +25,10 @@ void
 LogFmt(const S &format_str, Args&&... args) noexcept
 {
 #if FMT_VERSION >= 90000
-	return LogVFmt(format_str,
-		       fmt::make_format_args(args...));
+  return LogVFmt(format_str, fmt::make_format_args(args...));
 #else
-	return LogVFmt(fmt::to_string_view(format_str),
-		       fmt::make_args_checked<Args...>(format_str,
-						       args...));
+  return LogVFmt(fmt::to_string_view(format_str),
+                 fmt::make_args_checked<Args...>(format_str, args...));
 #endif
 }
 
